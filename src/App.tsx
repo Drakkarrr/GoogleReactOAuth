d45be68b670a1a44dcb4a35198a1dc8d0c8f6c51
Junrey Lañas <jrydev404@gmail.com>
2022-09-04 18:28:25 +0800

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <h1>La photographia initial setup</h1>
+    </div>
+  )
+}
+
+export default App
