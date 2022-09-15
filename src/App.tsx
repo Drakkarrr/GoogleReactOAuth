bc27ee82dd7d3fce6da7660f4a772bc7339e36bb
Junrey Lañas <jrydev404@gmail.com>
2022-09-15 17:38:10 +0800

    install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import './styles/App.css'
 import Header from './components/Header'
+import Button from '@mui/material/Button'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <h1>La photographia initial setup</h1>
+      <Button variant='contained'>Hello World</Button>
+      <button className='bg-black w-25 hover:bg-blue-700 h-10 border-blue-700 rounded'>
+        Button
+      </button>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Header: React.FC = () => {
-    return (
-        <h1>Hello Header!</h1>
-    )
+  return (
+    <>
+      <h1>Hello HEader!</h1>
+
+      <div className='bg-black min-h-80 flex justify-center items-center'>
+        <h3 className='text-3xl font-bold text-blue-600'>HEllo peanut!</h3>
+      </div>
+    </>
+  )
 }
 
 export default Header
