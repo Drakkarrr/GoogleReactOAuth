2631de8875017c994b8533b90104d2020885527b
Junrey Lañas <jrydev404@gmail.com>
2022-10-08 16:44:40 +0800

    Adjusted massonry

--- src/components/home/Masonry.tsx
+++ src/components/home/Masonry.tsx
@@ -8,21 +8,36 @@ const Masonry = (): JSX.Element => {
   return (
     <>
       <div className='container mx-auto my-10 py-20'>
-        <div className='gap-y-10 columns-3'>
+        <div className='gap-6 columns-3'>
           <img className='w-full h-80' src={test} />
-          <img className='aspect-auto w-full h-80' src={ImagePlaceholder} />
-          <img className='aspect-video w-full h-80' src={test} />
-          <img className='aspect-square w-100 h-auto' src={ImagePlaceholder} />
-          <img className='aspect-square w-full h-auto' src={test2} />
-          <img className='aspect-square w-full h-80' src={ImagePlaceholder} />
-          <img className='aspect-square w-full h-auto' src={test2} />
-          <img className='aspect-square w-full h-auto' src={test2} />
-          <img className='aspect-video w-full h-80' src={test} />
-          <img className='aspect-square w-full h-auto' src={ImagePlaceholder} />
-          <img className='aspect-square w-full h-auto' src={test3} />
-          <img className='aspect-square w-full h-auto' src={test2} />
-          <img className='aspect-square w-full h-auto' src={ImagePlaceholder} />
-          <img className='aspect-square w-full h-auto' src={test} />
+          <img
+            className='aspect-auto w-full h-80 mt-10'
+            src={ImagePlaceholder}
+          />
+          <img className='aspect-video w-full h-80 mt-6' src={test} />
+          <img
+            className='aspect-square w-100 h-auto mt-6'
+            src={ImagePlaceholder}
+          />
+          <img className='aspect-square w-full h-auto mt-7' src={test2} />
+          <img
+            className='aspect-square w-full h-80 mt-3'
+            src={ImagePlaceholder}
+          />
+          <img className='aspect-square w-full h-auto mt-4' src={test2} />
+          <img className='aspect-square w-full h-auto mt-6' src={test2} />
+          <img className='aspect-video w-full h-80 mt-3' src={test} />
+          <img
+            className='aspect-square w-full h-auto mt-7'
+            src={ImagePlaceholder}
+          />
+          <img className='aspect-square w-full h-auto mt-10' src={test3} />
+          <img className='aspect-square w-full h-auto mt-8' src={test2} />
+          <img
+            className='aspect-square w-full h-auto mt-6'
+            src={ImagePlaceholder}
+          />
+          <img className='aspect-square w-full h-auto mt-8' src={test} />
         </div>
       </div>
     </>
